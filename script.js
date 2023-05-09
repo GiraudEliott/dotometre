89b3aa66f0c6aedd6b79fd60e42ceaf3b02e5c3d
Eliott Giraud <98514128+DiableEliott@users.noreply.github.com>
2023-05-09 22:06:04 +0200

    push 1 test

--- script.js
+++ script.js
@@ -2,79 +2,48 @@ var messageList = document.getElementById("messageList");
 var timerElement = document.getElementById("timer");
 
 function startTimer() {
-	var seconds = 0;
-	var intervalId = setInterval(function() {
-		seconds++;
-		var minutes = Math.floor(seconds / 60);
-		var remainingSeconds = seconds % 60;
-		timerElement.innerHTML = formatTime(minutes) + ":" + formatTime(remainingSeconds);
-		
-        if (seconds % 120 === 0) {
-			addMessageToList("RIVER RUNE");
-		}
+    var delaySeconds = 0; //DELAI ICI
+    var seconds = 0;
+    var intervalId;
 
-        if (seconds % 180 === 0) {
-			addMessageToList("LOTUS");
-		}
-        
-        if (seconds % 180 === 0) {
-            addMessageToList("BOUNTY RUNES");
-		}
-        
-        if (seconds % 420 === 0) {
-            addMessageToList("WISDOM RUNE");
-        }
+    function startInterval() {
+        intervalId = setInterval(function() {
+            seconds++;
+            var minutes = Math.floor(seconds / 60);
+            var remainingSeconds = seconds % 60;
+            timerElement.innerHTML = formatTime(minutes) + ":" + formatTime(remainingSeconds);
 
-        if (seconds % 1200 === 0) {
-            addMessageToList("TORMENTOR");
-        }
-	}, 1000);
-}
+            if (seconds % 2 === 0) {
+                addMessageToList("RIVER RUNE");
+            }
 
-var messageList = document.getElementById("messageList");
-var timerElement = document.getElementById("timer");
+            if (seconds % 8 === 0) {
+                addMessageToList("LOTUS");
+            }
 
-function startTimer() {
-	var seconds = 0;
-	var intervalId = setInterval(function() {
-		seconds++;
-		var minutes = Math.floor(seconds / 60);
-		var remainingSeconds = seconds % 60;
-		timerElement.innerHTML = formatTime(minutes) + ":" + formatTime(remainingSeconds);
-		
-        if (seconds % 2 === 0) {
-			addMessageToList("RIVER RUNE");
-		}
+            if (seconds % 180 === 0) {
+                addMessageToList("BOUNTY RUNES");
+            }
 
-        if (seconds % 4 === 0) {
-			addMessageToList("LOTUS");
-		}
-        
-        if (seconds % 8 === 0) {
-            addMessageToList("BOUNTY RUNES");
-		}
-        
-        if (seconds % 13 === 0) {
-            addMessageToList("WISDOM RUNE");
-        }
+            if (seconds % 420 === 0) {
+                addMessageToList("WISDOM RUNE");
+            }
 
-        if (seconds % 1200 === 0) {
-            addMessageToList("TORMENTOR");
-        }
-	}, 1000);
+            if (seconds % 1200 === 0) {
+                addMessageToList("TORMENTOR");
+            }
+        }, 1000);
+    }
+
+    setTimeout(startInterval, delaySeconds * 1000);
 }
 
 function addMessageToList(message) {
-	var li = document.createElement("li");
-	li.innerHTML = message;
-	messageList.insertBefore(li, messageList.firstChild);
+    var li = document.createElement("li");
+    li.innerHTML = message;
+    messageList.insertBefore(li, messageList.firstChild);
 }
 
 function formatTime(time) {
-	return time < 10 ? "0" + time : time;
-}
-
-
-function formatTime(time) {
-	return time < 10 ? "0" + time : time;
+    return time < 10 ? "0" + time : time;
 }
